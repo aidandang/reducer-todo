83b1c2ccc57c2d6fee8aa58788d91247b68011ab
Aidan Dang <aidandang@gmail.com>
2020-05-20 20:15:07 -0700

    created a sample default reducer.js and its initialState of todo list

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,26 @@
+export const initialState = [
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+  },
+  {
+    item: 'Study Redux',
+    completed: false,
+    id: 3892987589
+  },
+  {
+    item: 'Read primative types and Objects in JavaScript',
+    completed: false,
+    id: 3892987589
+  },
+  {
+    item: 'Complete the project',
+    completed: false,
+    id: 3892987589
+  }
+]
+
+export const reducer = () => {
+  return "";
+}
